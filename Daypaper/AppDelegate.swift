5ddd9384f7515d54212934c86b1585f21836cc4e
Andrey Maksimov <meamka@ya.ru>
2018-01-16 03:11:44 +0300

    Add Pods and basic download feature

--- Daypaper/AppDelegate.swift
+++ Daypaper/AppDelegate.swift
@@ -7,14 +7,16 @@
 //
 
 import Cocoa
-
-let BASE_URL: String = "https://yandex.com/images/";
+import Nuke
+import SwiftDate
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDelegate, NSOpenSavePanelDelegate {
 
     @IBOutlet var StatusMenu: NSMenu!
     @IBOutlet weak var photoLabel: NSMenuItem!
+    
+    let BASE_URL: String = "https://yandex.com/images/"
     
     var statusItem : NSStatusItem!
     var prefs : UserDefaults!
@@ -31,6 +33,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
         initDownloadFolder()
         
         // Finally we should init timer
+        initWpTimer()
     }
     
     func initStatusMenu() {
@@ -57,11 +60,13 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
     }
     
     func initWpTimer() {
-        wpTimer = Timer.scheduledTimer(timeInterval: prefs.double(forKey: "check_interval"),
+        let interval = prefs.double(forKey: "check_interval")
+        wpTimer = Timer.scheduledTimer(timeInterval: interval,
                                        target: self,
-                                       selector: (#selector(checkWallpaper)),
+                                       selector: (#selector(beginUpdateWallpaper)),
                                        userInfo: nil,
                                        repeats: true)
+        print("WPTimer initialized with interval \(interval)")
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -72,43 +77,44 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
     }
     
     @IBAction func downloadTodayClicked(_ sender: Any) {
-        downloadImage()
+        downloadImage(force: true)
     }
     
     func initDownloadFolder() {
-        if (prefs.string(forKey: "download_folder") == nil)
-        {
-            print("Should create or select download folder")
-            
-            let notification = NSUserNotification.init()
-            notification.title = "Daypaper"
-            notification.informativeText = "Download folder not set"
-            notification.actionButtonTitle = "Use Default"
-            notification.otherButtonTitle = "Select"
-            notification.hasActionButton = true
-            notification.hasReplyButton = true
-            notification.soundName = "Glass"
-            notification.userInfo = ["action": "download_folder"]
-            NSUserNotificationCenter.default.delegate = self
-            NSUserNotificationCenter.default.deliver(notification)
-            
-        } else {
-            if (FileManager.default.isWritableFile(atPath: prefs.string(forKey: "download_folder")!)) {
-                initWpTimer()
-            } else {
-                let notification = NSUserNotification.init()
-                notification.title = "Daypaper"
-                notification.informativeText = "Download folder are not writable"
-                notification.actionButtonTitle = "Use Default"
-                notification.otherButtonTitle = "Select another"
-                notification.hasActionButton = true
-                notification.hasReplyButton = true
-                notification.soundName = "Glass"
-                notification.userInfo = ["action": "download_folder"]
-                NSUserNotificationCenter.default.delegate = self
-                NSUserNotificationCenter.default.deliver(notification)
-            }
-        }
+        let defaultPath = makeDefaultPath()
+        print("Prefs download_folder -> \(defaultPath)")
+//        if (prefs.string(forKey: "download_folder") == nil)
+//        {
+//            print("Should create or select download folder")
+//
+//            let notification = NSUserNotification.init()
+//            notification.title = "Daypaper"
+//            notification.informativeText = "Download folder not set"
+//            notification.actionButtonTitle = "Use Default"
+//            notification.otherButtonTitle = "Select"
+//            notification.hasActionButton = true
+//            notification.hasReplyButton = true
+//            notification.userInfo = ["action": "download_folder"]
+//            NSUserNotificationCenter.default.delegate = self
+//            NSUserNotificationCenter.default.deliver(notification)
+//
+//        } else {
+//            if (FileManager.default.isWritableFile(atPath: prefs.string(forKey: "download_folder")!)) {
+//                initWpTimer()
+//            } else {
+//                let notification = NSUserNotification.init()
+//                notification.title = "Daypaper"
+//                notification.informativeText = "Download folder are not writable"
+//                notification.actionButtonTitle = "Use Default"
+//                notification.otherButtonTitle = "Select another"
+//                notification.hasActionButton = true
+////                notification.hasReplyButton = true
+////                notification.soundName = "Glass"
+//                notification.userInfo = ["action": "download_folder"]
+//                NSUserNotificationCenter.default.delegate = self
+//                NSUserNotificationCenter.default.deliver(notification)
+//            }
+//        }
     }
     
     func userNotificationCenter(_ center: NSUserNotificationCenter, didActivate notification: NSUserNotification) {
@@ -117,22 +123,27 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
                 guard let res = notification.response else { return }
                 print("User replied: \(res.string)")
             default:
+                selectDownloadFolder()
                 print("didActivate userInfo -> \(notification.userInfo!)")
                 break;
         }
     }
     
-    func userNotificationCenter(_ center: NSUserNotificationCenter, didDeliver notification: NSUserNotification) {
-        print("didDeliver userInfo -> \(notification.userInfo!)")
-        if (notification.userInfo != nil) {
-            switch (notification.userInfo!["action"]) {
-            default:
-                let path = makeDefaultPath()
-                makeWallpaperFolder(path)
-                break;
-            }
-        }
-    }
+//    func userNotificationCenter(_ center: NSUserNotificationCenter, didDeliver notification: NSUserNotification) {
+//        print("didDeliver userInfo -> \(notification.userInfo!)")
+//        if (notification.userInfo != nil) {
+//            switch notification.userInfo!["action"] as! String {
+//                case "download_folder":
+//                    print("download_folder")
+//                    selectDownloadFolder()
+//                    break
+//                default:
+//                    let path = makeDefaultPath()
+//                    makeWallpaperFolder(path)
+//                    break;
+//            }
+//        }
+//    }
     
     func userNotificationCenter(_ center: NSUserNotificationCenter, shouldPresent notification: NSUserNotification) -> Bool {
         return prefs.bool(forKey: "show_notifications") != false
@@ -141,8 +152,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
     func makeDefaultPath() -> String {
         var path = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.picturesDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)[0] as String
         path.append("/Daypaper")
-        print(path)
-//        prefs.set(path, forKey: "download_folder")
+        prefs.set(path, forKey: "download_folder")
         return path
     }
     
@@ -162,23 +172,110 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
         }
     }
     
-    @objc func checkWallpaper() {
-        print("Checking wallpaper…")
+    func selectDownloadFolder() {
+        
+        let panel = NSOpenPanel.init()
+        panel.title = "Select Wallpaper's folder"
+        panel.titleVisibility = .visible
+        panel.canChooseDirectories = true
+        panel.canChooseFiles = false
+        panel.allowsMultipleSelection = false
+        panel.canCreateDirectories = true
+        panel.delegate = self
+        panel.begin(completionHandler: { (result) in
+            if (result == NSApplication.ModalResponse.OK) {
+                let path = panel.url!.path
+                self.prefs.set(path, forKey: "download_folder")
+//                self.prefs.synchronize()
+                print(self.prefs.string(forKey: "download_folder")!)
+            }
+        })
     }
     
-    func downloadImage() {
-        NSScreen.screens.forEach { (screen) in
-            print("Begin download wallpaper for \(getScreenSize(screen: screen))")
-            applyWallpaper(screen: screen)
+    @objc func beginUpdateWallpaper() {
+        downloadImage(force: false)
+    }
+    
+    func checkWallpaper(localFilename: String) -> Bool {
+        print("Checking wallpaper…")
+        if (!FileManager.default.fileExists(atPath: localFilename)) {
+            return true
+        } else {
+            NSLog("File %@ already exists.", localFilename)
+            return false
         }
     }
     
-    func applyWallpaper(screen: NSScreen) {
-        print("Applying wallpaper to \(getScreenSize(screen: screen))")
+    func downloadImage(force: Bool?) {
+        NSScreen.screens.forEach { (screen) in
+            let (sWidth, sHeight) = getScreenSize(screen: screen)
+            let downloadURL = URL.init(string: "\(BASE_URL)today?size=\(sWidth)x\(sHeight)")
+            let localURL = URL.init(fileURLWithPath: makeLocalFilename(width: sWidth, height: sHeight))
+
+            print("Begin download wallpaper from \(downloadURL!)")
+            Manager.shared.loadImage(with: downloadURL!, completion: { (result) in
+                if(result.error != nil) {
+                    print(result.error!)
+                } else {
+                    let wp = result.value!
+                    do {
+                        try wp.tiffRepresentation?.write(to: localURL, options: Data.WritingOptions.atomicWrite)
+                        print("Saved to \(localURL.absoluteString)")
+                        
+                        if (self.prefs.bool(forKey: "download_only") == true) {
+                            self.applyWallpaper(screen: screen, wallpaperURL: localURL)
+                        }
+                        
+                    } catch let error as NSError {
+                        print(error.localizedDescription)
+                    }
+                }
+            })
+        }
     }
     
+    /**
+     Apply Wallpaper to given screen
+    */
+    func applyWallpaper(screen: NSScreen, wallpaperURL: URL) {
+        print("Applying wallpaper to \(getScreenSize(screen: screen))")
+        
+        do {
+            try NSWorkspace.shared.setDesktopImageURL(wallpaperURL, for: screen, options: [:])
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+    
+    /**
+     Detect given screen width & height in pixels.
+     
+     - returns:
+     Tuple of Width and Height represented as Ints
+     
+     - parameters:
+        - screen: NSScreen object to detect attributes
+     
+     - author:
+     Andrey Maksimov
+     
+     - version:
+     0.1
+    */
     func getScreenSize(screen: NSScreen) -> (Int, Int) {
         return (Int(screen.frame.width), Int(screen.frame.height))
+    }
+    
+    @IBAction func viewOnYandexClicked(_ sender: Any) {
+        let url = URL.init(string: BASE_URL)!
+        NSWorkspace.shared.open(url)
+    }
+    
+    func makeLocalFilename(width: Int, height: Int) -> String {
+        let download_folder = prefs.string(forKey: "download_folder") ?? ""
+        let date = DateInRegion()
+        let dateString = date.string(format: .iso8601(options: .withFullDate))
+        return "\(download_folder)/\(dateString)-\(width)x\(height).jpg"
     }
 }
 
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+platform :osx, '10.11'
+
+target 'Daypaper' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Daypaper
+  pod 'Nuke', '~> 6.0'
+  pod 'SwiftDate'
+
+  target 'DaypaperTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'DaypaperUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
